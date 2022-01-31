d3075fa61e6519144afc3dbff0c91783204a915c
Bergen Pohl <circidalia@gmail.com>
2022-01-30 19:04:35 -0500

    Added aff_a.c

---
+++ 00/0-0-aff_a/aff_a.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+int aff(char *arg) {
+	while (*arg != '\0') {
+		if (*arg == 'a') return 1;
+		arg++;
+	}
+	return 0;
+}
+
+int main(int argc, char *argv[]) {
+	if (argc != 2 || aff(argv[1])) {
+		write(1, "a", 1);
+	}
+	write(1, "\n", 1);
+	return 0;
+}
