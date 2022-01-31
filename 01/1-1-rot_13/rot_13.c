49f08a54d7bc5ee65f29d9c7f277420249354242
Bergen Pohl <circidalia@gmail.com>
2022-01-31 12:26:52 -0500

    Added rot_13.c

---
+++ 01/1-1-rot_13/rot_13.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+
+void rot_13(char *arg) {
+	char c;
+	
+	while (*arg != '\0') {
+		c = *arg;
+		if (c >= 'a' && c <= 'z') {
+			c = 'a' + (((c - 'a') + 13) % 26);
+		} else if (c >= 'A' && c <= 'Z') {
+			c = 'A' + (((c - 'A') + 13) % 26);
+		}
+		write(1, &c, 1);
+		arg++;
+	}
+}
+
+int main(int argc, char*argv[]) {
+	if (argc == 2) rot_13(argv[1]);
+	write(1, "\n", 1);
+	return 0;
+}
