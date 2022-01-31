b02d853d5934af4cf3d0165a6ad38df5a5458711
Bergen Pohl <circidalia@gmail.com>
2022-01-31 12:18:15 -0500

    Added ulstr.c

---
+++ 01/1-0-ulstr/ulstr.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+
+void ulstr(char *str) {
+	char c;
+	
+	while (*str != '\0') {
+		c = *str;
+		if (c >= 'a' && c <= 'z') {
+			c -= 32;
+		} else if(c >= 'A' && c <= 'Z') {
+			c += 32;
+		}
+		write(1, &c, 1);
+		str++;
+	}
+}
+
+int main(int argc, char *argv[]) {
+	if (argc == 2) ulstr(argv[1]);
+	write(1, "\n", 1);
+	return 0;
+}
