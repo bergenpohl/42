d7e6b7e7df2be27a370e5238b90ffc3d4369313f
Bergen Pohl <circidalia@gmail.com>
2022-01-30 19:39:47 -0500

    Added maff_revalpha.c

---
+++ 00/0-1-maff_revalpha/maff_revalpha.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+int main() {
+	int i = 25;
+	char c;
+
+	while (i >= 0) {
+		c = 'a' + i;
+		if (i % 2 == 0) c -= 32;
+		write(1, &c, 1);
+		i--;
+	}
+	write(1, "\n", 1);
+	return 0;
+}
