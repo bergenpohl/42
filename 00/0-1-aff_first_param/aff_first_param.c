676585b0ce62c521c0a440bb2b3cb279302ed599
Bergen Pohl <circidalia@gmail.com>
2022-01-30 19:30:29 -0500

    Added aff_first_param.c

---
+++ 00/0-1-aff_first_param/aff_first_param.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+
+size_t ft_strlen(char *str) {
+	size_t i;
+
+	i = 0;
+	while (str[i] != '\0') {
+		i++;
+	}
+	return i;
+}
+
+void aff_first_param(char *arg) {
+	write(1, arg, ft_strlen(arg));
+}
+
+int main(int argc, char *argv[]) {
+	if (argc > 1) aff_first_param(argv[1]);
+	write(1, "\n", 1);
+	return 0;
+}
