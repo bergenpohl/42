8c25805ddb49c2b2174bf5f6e15a648e028877ae
Bergen Pohl <circidalia@gmail.com>
2022-02-01 12:36:29 -0500

    Added first_word.c

---
+++ 01/1-2-first_word/first_word.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+void first_word(char *arg) {
+	while (*arg != '\0') {
+		if (*arg != '\t' && *arg != ' ') {
+			while (*arg != '\t' && *arg != ' ' && *arg != '\0') {
+				write(1, arg, 1);
+				arg++;
+			}
+			return;
+		}
+		arg++;
+	}
+}
+
+int main(int argc, char *argv[]) {
+	if (argc == 2) first_word(argv[1]);
+	write(1, "\n", 1);
+	return 0;
+}
