bd6c066b918ffd6da0cf91b5f830f4da7c254143
Bergen Pohl <circidalia@gmail.com>
2022-02-01 20:08:03 -0500

    Added rev_print.c

---
+++ 01/1-3-rev_print/rev_print.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+
+void rev_print(char *arg) {
+	size_t i;
+
+	i = 0;
+	while (arg[i] != '\0') {
+		i++;
+	}
+	while (i--) {
+		write(1, arg + i, 1);
+	}
+}
+
+int main(int argc, char *argv[]) {
+	if (argc == 2) rev_print(argv[1]);
+	write(1, "\n", 1);
+	return 0;
+}
