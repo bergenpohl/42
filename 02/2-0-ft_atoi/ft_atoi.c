da3b3c38a6938bec0d30f17a260e77c8f87e1006
Bergen Pohl <circidalia@gmail.com>
2022-02-02 07:18:01 -0500

    Added ft_atoi.c

---
+++ 02/2-0-ft_atoi/ft_atoi.c
@@ -0,0 +1,22 @@
+int ft_atoi(const char *str) {
+	int i;
+	int sign;
+
+	i = 0;
+	while (*str != '\0' && (*str == ' ' || *str == '\t' || *str == '\n' || *str == '\r' || *str == '\v' || *str == '\f')) {
+		str++;
+	}
+	sign = 1;
+	if (*str == '-') {
+		str++;
+		sign = -1;
+	} else if (*str == '+') {
+		str++;
+	}
+	while (*str >= '0' && *str <= '9') {
+		i *= 10;
+		i += (*str - '0');
+		str++;
+	}
+	return i * sign;
+}
