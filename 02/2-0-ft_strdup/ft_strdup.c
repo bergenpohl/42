3c8f17ab8ded714bbb57cdf32ef6b05f5835d690
Bergen Pohl <circidalia@gmail.com>
2022-02-03 16:50:44 -0500

    Added ft_strdup.c

---
+++ 02/2-0-ft_strdup/ft_strdup.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+char    *ft_strdup(char *src) {
+	size_t i;
+	char *ret;
+	
+	i = 0;
+	while (src[i] != '\0') {
+		++i;
+	}
+	ret = (char *)malloc(i + 1);
+	if (ret == NULL) return NULL;
+	while(i) {
+		ret[i] = src[i];
+		i--;
+	}
+	ret[0] = src[0];
+	return ret;
+}
