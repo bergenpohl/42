bbca802d9472a1a76fdbf4f3373d7c04fd65c39e
Bergen Pohl <circidalia@gmail.com>
2022-02-03 17:48:32 -0500

    Added reverse_bits.c

---
+++ 02/2-0-reverse_bits/reverse_bits.c
@@ -0,0 +1,14 @@
+unsigned char	reverse_bits(unsigned char octet) {
+	unsigned char r;
+	int i;
+
+	r = 0;
+	i = 0;
+	while (i < 8) {
+		r = r << 1;
+		r += octet & 1;
+		octet = octet >> 1;
+		i++;
+	}
+	return r;
+}
