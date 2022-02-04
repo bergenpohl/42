ed72ab3b42fbf5ba3927fab5cde3ef78930d0572
Bergen Pohl <circidalia@gmail.com>
2022-02-04 08:26:46 -0500

    Added print_bits.c

---
+++ 02/2-4-print_bits/print_bits.c
@@ -0,0 +1,12 @@
+void	printbits(unsigned char octet) {
+	unsigned char i;
+	char cs[8];
+	
+	i = 0;
+	while (i < 8) {
+		cs[i] = '0' + (octet & 128 ? 1 : 0);
+		octet = octet << 1;
+		i++;
+	}
+	write(1, cs, 8);
+}
