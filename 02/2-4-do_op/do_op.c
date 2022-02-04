291d2a5cd8218faa265020cfb93b68e307f15f56
Bergen Pohl <circidalia@gmail.com>
2022-02-04 08:23:01 -0500

    Added do_op.c

---
+++ 02/2-4-do_op/do_op.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void do_op(char *a, char *b, char *c) {
+	int i;
+	int j;
+	int res;
+	
+	i = atoi(a);
+	j = atoi(c);
+	if (*b == '+') {
+		res = i + j;
+	} else if (*b == '-') {
+		res = i - j;
+	} else if (*b == '*') {
+		res = i * j;
+	} else if (*b == '/') {
+		res = i / j;
+	} else {
+		res = i % j;
+	}
+	printf("%d\n", res);
+}
+
+int main(int argc, char *argv[]) {
+	if (argc == 4) do_op(argv[1], argv[2], argv[3]);
+	else write(1, "\n", 1);
+	return 0;
+}
