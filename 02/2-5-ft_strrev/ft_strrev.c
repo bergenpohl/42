b2973a746031d3b6783da8376400449a46912eec
Bergen Pohl <circidalia@gmail.com>
2022-02-04 08:48:28 -0500

    Added ft_strrev.c

---
+++ 02/2-5-ft_strrev/ft_strrev.c
@@ -0,0 +1,18 @@
+char    *ft_strrev(char *str) {
+	size_t i, j, len;
+	char c;
+	
+	len = 0;
+	while (str[len] != '\0') {
+		len++;
+	}
+	i = len;
+	j = 0;
+	while (i-- > (len / 2)) {
+		c = str[i];
+		str[i] = str[j];
+		str[j] = c;
+		j++;
+	}
+	return str;
+}
