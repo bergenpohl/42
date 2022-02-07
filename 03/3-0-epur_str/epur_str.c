34ee98485d627dbebcc52d11ec2a44d2be4b9ac1
Bergen Pohl <circidalia@gmail.com>
2022-02-06 19:52:19 -0500

    Added epur_str.c

---
+++ 03/3-0-epur_str/epur_str.c
@@ -0,0 +1,33 @@
+#include <unistd.h>
+
+void epur_str(char *str) {
+	size_t i;
+
+	while (*str == ' ' || *str == '\t') {
+		str++;
+	}
+	i = 0;
+	while (str[i] != ' ' && str[i] != '\t' && str[i] != '\0') {
+		i++;
+	}
+	write(1, str, i);
+	str += i;
+	while (*str != '\0') {
+		while (*str == ' ' || *str == '\t') {
+			str++;
+		}
+		i = 0;
+		while (str[i] != ' ' && str[i] != '\t' && str[i] != '\0') {
+			i++;
+		}
+		if (i > 0) write(1, " ", 1);
+		write(1, str, i);
+		str += i;
+	}
+}
+
+int main(int argc, char *argv[]) {
+	if (argc == 2) epur_str(argv[1]);
+	write(1, "\n", 1);
+	return 0;
+}
