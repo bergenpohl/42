e485df1f3adea46860a013ec1fcd53faa0ec89a9
Bergen Pohl <circidalia@gmail.com>
2022-02-06 20:03:09 -0500

    Added ft_list_size.c & ft_list.h

---
+++ 03/3-0-ft_list_size/ft_list_size.c
@@ -0,0 +1,12 @@
+#include "ft_list.h"
+
+int	ft_list_size(t_list *begin_list) {
+	int len;
+	
+	len = 0;
+	while (begin_list != NULL) {
+		len++;
+		begin_list = begin_list->next;
+	}
+	return len;
+}
