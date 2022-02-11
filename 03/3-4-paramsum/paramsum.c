c79842620b0b11df83ddbd455a686bc4466158cf
Bergen Pohl <circidalia@gmail.com>
2022-02-11 10:52:46 -0500

    Added paramsum.c

---
+++ 03/3-4-paramsum/paramsum.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+
+void iout(int i) {
+	int q;
+	int n;
+	char c;
+	
+	n = i / 10;
+	q = 1;
+	while (n) {
+		q *= 10;
+		n /= 10;
+	}
+	while (q) {
+		c = '0' + (i / q);
+		i = i % q;
+		q /= 10;
+		write(1, &c, 1);
+	}
+}
+
+void paramsum(int i) {
+	iout(i);
+	write(1, "\n", 1);
+}
+
+int main(int argc, char *argv[]) {
+	paramsum(argc - 1);
+	return 0;
+}
