9113f4e47d1151e2b00387367fd159ea7d4d81a9
Bergen Pohl <bergenpohl@gmail.com>
2022-02-12 19:55:07 -0500

    Added ft_range.c

---
+++ 03/3-3-ft_range/ft_range.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+
+int *ft_range(int start, int end) {
+	int *range;
+	int i;
+
+	i = start;
+	if (start < end) {
+		range = (int *)malloc((end - start) + 1);
+		while (i <= end) {
+			range[i - start] = i;
+			i++;
+		}
+	} else {
+		range = (int *)malloc((start - end) + 1);
+		while (i >= end) {
+			range[start - i] = i;
+			i--;
+		}
+	}
+	return range;
+}
