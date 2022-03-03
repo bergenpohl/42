f919232024bc19ffe41624c25a4840ddebf7efb2
Bergen Pohl <circidalia@gmail.com>
2022-03-02 21:31:13 -0500

    Update epur_str.c

--- 03/3-0-epur_str/epur_str.c
+++ 03/3-0-epur_str/epur_str.c
@@ -1,6 +1,6 @@
 #include <unistd.h>
 
-void epur_str(char *str) {
+void	epur_str(char *str) {
 	size_t i;
 
 	while (*str == ' ' || *str == '\t') {
@@ -26,7 +26,7 @@ void epur_str(char *str) {
 	}
 }
 
-int main(int argc, char *argv[]) {
+int	main(int argc, char *argv[]) {
 	if (argc == 2) epur_str(argv[1]);
 	write(1, "\n", 1);
 	return 0;
