76e8d506d0b4136a05b235d5f9f7e2b0ab7597ae
Bergen Pohl <circidalia@gmail.com>
2022-03-02 21:34:45 -0500

    Update expand_str.c

--- 03/3-1-expand_str/expand_str.c
+++ 03/3-1-expand_str/expand_str.c
@@ -1,6 +1,6 @@
 #include <unistd.h>
 
-void expand_str(char *str) {
+void	expand_str(char *str) {
 	int is;
 	
 	is = 0;
@@ -10,9 +10,7 @@ void expand_str(char *str) {
 				str++;
 			}
 			if (*str == '\0') return;
-			if (is) {
-				write(1, "   ", 3);
-			}
+			if (is) write(1, "   ", 3);
 		}
 		is = 1;
 		write(1, str, 1);
@@ -20,7 +18,7 @@ void expand_str(char *str) {
 	}
 }
 
-int main(int argc, char *argv[]) {
+int	main(int argc, char *argv[]) {
 	if (argc == 2) expand_str(argv[1]);
 	write(1, "\n", 1);
 	return 0;
