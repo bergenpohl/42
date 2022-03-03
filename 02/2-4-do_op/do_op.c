4b335b6d1c64c6d3770411c774c6ecb04ad62d93
Bergen Pohl <circidalia@gmail.com>
2022-03-03 16:11:37 -0500

    Update do_op.c

--- 02/2-4-do_op/do_op.c
+++ 02/2-4-do_op/do_op.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-void do_op(char *a, char *b, char *c) {
+void	do_op(char *a, char *b, char *c) {
 	int i;
 	int j;
 	int res;
@@ -23,7 +23,7 @@ void do_op(char *a, char *b, char *c) {
 	printf("%d\n", res);
 }
 
-int main(int argc, char *argv[]) {
+int	main(int argc, char *argv[]) {
 	if (argc == 4) do_op(argv[1], argv[2], argv[3]);
 	else write(1, "\n", 1);
 	return 0;
