baf0dde7339359968a712ed95761043dca6019e8
Bergen Pohl <circidalia@gmail.com>
2022-03-02 21:42:33 -0500

    Update aff_last_param.c

--- 00/0-1-aff_last_param/aff_last_param.c
+++ 00/0-1-aff_last_param/aff_last_param.c
@@ -1,6 +1,6 @@
 #include <unistd.h>
 
-size_t ft_strlen(char *str) {
+size_t	ft_strlen(char *str) {
 	size_t i;
 
 	i = 0;
@@ -10,11 +10,11 @@ size_t ft_strlen(char *str) {
 	return i;
 }
 
-void aff_last_param(char *arg) {
+void	aff_last_param(char *arg) {
 	write(1, arg, ft_strlen(arg));
 }
 
-int main(int argc, char *argv[]) {
+int	main(int argc, char *argv[]) {
 	if (argc > 1) aff_last_param(argv[argc - 1]);
 	write(1, "\n", 1);
 	return 0;
