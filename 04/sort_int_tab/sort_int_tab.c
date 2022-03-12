6448b0e37b0298875ab114de99449492982e3bc7
Bergen Pohl <circidalia@gmail.com>
2022-03-11 19:07:30 -0500

    Added sort_int_tab.c

--- 04/sort_int_tab/sort_int_tab.c
+++ 04/sort_int_tab/sort_int_tab.c
@@ -1,3 +1,17 @@
 void	sort_int_tab(int *tab, unsigned int size) {
+	int i;
+	int t;
 
+	i = 0;
+	while (i < size - 1) {
+		if (tab[i] > tab[i + 1]) {
+			t = tab[i];
+			tab[i] = tab[i + 1];
+			tab[i + 1] = t;
+			if (i != 0) i--;
+			else i++;
+		} else {
+			i++;
+		}
+	}
 }
