d60ec4f993a36ffb7bbdd3cf34845d6c8e44bfc9
Bergen Pohl <circidalia@gmail.com>
2022-03-15 18:10:22 -0400

    Added cycle_detector

--- 05/cycle_detector/cycle_detector.c
+++ 05/cycle_detector/cycle_detector.c
@@ -1,3 +1,24 @@
+#include <stdlib.h>
+#include "list.h"
+
 int	cycle_detector(const t_list *list) {
-	
+	t_list	*p;
+	t_list	*q;
+	size_t	i;
+	size_t	len;
+
+	p = (t_list *)list;
+	len = 0;
+	while (p) {
+		i = 0;
+		q = (t_list *)list;
+		while (i < len) {
+			if (q == p) return 1;
+			i++;
+			q = q->next;
+		}
+		p = p->next;
+		len++;
+	}
+	return 0;
 }
