64c1bf391ad51fb749b1a493f4b1e454c62cb00f
Bergen Pohl <circidalia@gmail.com>
2022-03-17 23:19:25 -0400

    Bug fix and added commented out main

--- 01/1-0-ft_strcpy/ft_strcpy.c
+++ 01/1-0-ft_strcpy/ft_strcpy.c
@@ -4,5 +4,20 @@ char	*ft_strcpy(char *s1, char *s2) {
 		s1++;
 		s2++;
 	}
+	*s1 = *s2;
 	return s1;
 }
+
+/*
+#include <stdio.h>
+
+int	main(int argc, char *argv[]) {
+	char a[] = "Hello, world!\n";
+	char b[15];
+
+	ft_strcpy(b, a);
+	printf("%s", b);
+
+	return 0;
+}
+*/
