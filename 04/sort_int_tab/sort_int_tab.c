e693e10458da07052b5e8886c5080b49808a7553
Bergen Pohl <circidalia@gmail.com>
2022-03-22 22:43:43 -0400

    Commented main

--- 04/sort_int_tab/sort_int_tab.c
+++ 04/sort_int_tab/sort_int_tab.c
@@ -15,3 +15,28 @@ void	sort_int_tab(int *tab, unsigned int size) {
 		}
 	}
 }
+
+/*
+#include <stdio.h>
+
+int	main() {
+	int tab[] = {5, 3, 2, 4};
+	int tab_size;
+	int i;
+
+	tab_size = 4;
+	i = 0;
+	while (i < tab_size) {
+		printf("%d ", tab[i]);
+		i++;
+	}
+	puts("");
+	sort_int_tab(tab, tab_size);
+	i = 0;
+	while (i < tab_size) {
+		printf("%d ", tab[i]);
+		i++;
+	}
+	puts("");
+}
+*/
